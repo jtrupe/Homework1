80c45e46abc7302f9dd54ec7de25b4a1e4e26350
jtrupe <53407584+jtrupe@users.noreply.github.com>
2021-08-23 19:54:01 -0700

    add project part 2, printing ascii penrose triangle

---
+++ Part2/Program.cs
@@ -0,0 +1,46 @@
+﻿/// Chapter No. 1		Homework 1 Part 2
+/// File Name: Homework1.cs
+/// @author: Bill Gates
+/// Date:  August 24, 2015
+///
+/// Problem Statement: d
+/// 
+/// 
+/// 
+/// Overall Plan:
+/// 1) print an image line by line to the screen
+/// 2) ascii art of penrose triangle found at link below
+/// 3) https://mathhelpboards.com/threads/math-related-ascii-art.10321/
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(@"               *---*");
+            Console.WriteLine(@"              / \   \");
+            Console.WriteLine(@"             /   \   \");
+            Console.WriteLine(@"            /     \   \");
+            Console.WriteLine(@"           /   *   \   \");
+            Console.WriteLine(@"          /   / \   \   \");
+            Console.WriteLine(@"         /   /   \   \   \");
+            Console.WriteLine(@"        /   /   / \   \   \");
+            Console.WriteLine(@"       /   /   /   \   \   \");
+            Console.WriteLine(@"      /   /   /---------*   \");
+            Console.WriteLine(@"     /   /   /               \");
+            Console.WriteLine(@"    *   /   *-----------------*");
+            Console.WriteLine(@"     \ /                     /");
+            Console.WriteLine(@"      *---------------------*");
+
+        }
+    }
+}
